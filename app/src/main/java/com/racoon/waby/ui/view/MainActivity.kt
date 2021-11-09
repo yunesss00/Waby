7d8a5a3844085afee6996ae51f820adde4904f6e
yunesss00 <younessbb00@gmail.com>
2021-11-10 00:06:43 +0100

    estructurando el proyecto

--- app/src/main/java/com/racoon/waby/ui/view/MainActivity.kt
+++ app/src/main/java/com/racoon/waby/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.racoon.waby
+package com.racoon.waby.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.racoon.waby.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
